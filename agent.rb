19b80fac9df04a33f2096eb37d6234e3d6c12992
benjamin harmless huber <b.huber1@gmx.at>
2010-04-19 19:43:28 +0200

    classes yml config

--- agent.rb
+++ agent.rb
@@ -2,83 +2,92 @@ require 'rubygems'
 require 'net/http'
 require 'builder'
 require 'active_support'
+require 'yaml'
 
-# Need to be filled in
+config = YAML.load_file(File.join(File.dirname(__FILE__),'config.yml'))
 
-user_key = ""
-host_key = ""
+class Transmission
+  def initialize(*args)
+    options = args.extract_options!
+    raise ArgumentError if options[:key].nil?
+    raise ArgumentError if options[:host].nil?
+    raise ArgumentError if options[:transmit].nil?
 
-# Transmitlist
-transmit = [:cpu_usage, :mem_buffers, :mem_cached, :mem_usage, :mem_free, :disk_space]
+    @url = URI.parse('http://monitor.ttech.at/entries.xml')
+    @request = Net::HTTP::Post.new(url.path)
+    @request['Content-Type'] = "application/xml"
+    @request['X-Requested-With'] = "XmlHttpRequest"
+    @key = options[:key]
+    @host = options[:host]
+    @transmit = options[:transmit].map(&:to_sym)
+    @entries = []
+  end
 
-# Script parameter
-disks_regex = [/[sh]d[a-d][1-9]+/, %r(/dev/)]
+  def generate_xml
+    xml_output = ""
+    x = Builder::XmlMarkup.new(:target => xml_output, :indent => 0)
+    x.instruct!
+    x.request do
+      x.configuration do
+        x.version 1.0
+        x.key @key
+        x.host @host
+      end
+      for entry in @entries
+        x.entry do
+          x.key entry[:name].to_s
+          x.value entry[:value].to_f
+          x.max entry[:max].to_f if entry[:max]
+        end
+      end
+    end
+    @request.body = xml_output
+  end
 
-url = URI.parse('http://monitor.ttech.at/entries.xml')
-request = Net::HTTP::Post.new(url.path)
-request['Content-Type'] = "application/xml"
-request['X-Requested-With'] = "XmlHttpRequest"
+  def set_entries
+    if RUBY_PLATFORM =~ /linux/i
+      # cpu
+      if @transmit.include? :cpu_usage
+        cpu_usage = `cat /proc/loadavg`.split.first.to_f * 100
+        @entries << {:name => "cpu_usage", :value => cpu_usage, :max => 100}
+      end
 
+      # memory
+      unless (@transmit & [:mem_buffers, :mem_cached, :mem_usage, :mem_free]).empty?
+        memory_stats = `cat /proc/meminfo`.split("\n").inject({}){|r,x| k,v = x.split(":"); r[k.underscore.to_sym] = v.strip.split.first.to_i; r}
+        mem_default = {:max => memory_stats[:mem_total]}
+        @entries << {:name => "mem_free", :value => memory_stats[:mem_free]}.merge!(mem_default) if @transmit.include? :mem_free
+        @entries << {:name => "mem_usage", :value => (memory_stats[:mem_total] - memory_stats[:mem_free])}.merge!(mem_default) if @transmit.include? :mem_usage
+        @entries << {:name => "mem_cached", :value => memory_stats[:cached]}.merge!(mem_default) if @transmit.include? :mem_cached
+        @entries << {:name => "mem_buffers", :value => memory_stats[:buffers]}.merge!(mem_default) if @transmit.include? :mem_buffers
+      end
 
+      # disks
+      disks_regex = [/[sh]d[a-d][1-9]+/, %r(/dev/)]
+      if @transmit.include? :disk_space
+        disk_list = %x(df -P).split("\n")
+        disk_list.shift
+        disk_list.each do |line|
+          line = line.split
+          if disks_regex.detect{|x| line[0].to_s.match(x)}
+            @entries << {:name => "disk usge #{line[0]}", :value => line[2], :max => line[1]}
+          end
+        end
+      end
+    end
+  end
+
+  def send
+    # TODO Store for later use if entries were not created
+    Net::HTTP.start(@url.host, @url.port) {|http| http.request(@request) }
+  end
+end
 
 while(true)
-  xml_output = ""
-  entries = []
   GC.start
   sleep 60
-  # Scripts
-  # cpu
-  if transmit.include? :cpu_usage
-    cpu_usage = `cat /proc/loadavg`.split.first.to_f * 100
-    entries << {:name => "cpu_usage", :value => cpu_usage, :max => 100}
-  end
 
-  # memory
-  unless (transmit & [:mem_buffers, :mem_cached, :mem_usage, :mem_free]).empty?
-    memory_stats = `cat /proc/meminfo`.split("\n").inject({}){|r,x| k,v = x.split(":"); r[k.underscore.to_sym] = v.strip.split.first.to_i; r}
-    mem_default = {:max => memory_stats[:mem_total]}
-    entries << {:name => "mem_free", :value => memory_stats[:mem_free]}.merge!(mem_default) if transmit.include? :mem_free
-    entries << {:name => "mem_usage", :value => (memory_stats[:mem_total] - memory_stats[:mem_free])}.merge!(mem_default) if transmit.include? :mem_usage
-    entries << {:name => "mem_cached", :value => memory_stats[:cached]}.merge!(mem_default) if transmit.include? :mem_cached
-    entries << {:name => "mem_buffers", :value => memory_stats[:buffers]}.merge!(mem_default) if transmit.include? :mem_buffers
-  end
-
-  # disks
-  if transmit.include? :disk_space
-    disk_list = %x(df -P).split("\n")
-    disk_list.shift
-    disk_list.each do |line|
-      line = line.split
-      if disks_regex.detect{|x| line[0].to_s.match(x)}
-        entries << {:name => "disk usge #{line[0]}", :value => line[2], :max => line[1]}
-        #puts ["disk percent #{line[0]}", line[4].to_i]
-      end
-    end
-  end
-
-  # Add entries
-
-
-  # XML Request
-
-  x = Builder::XmlMarkup.new(:target => xml_output, :indent => 0)
-  x.instruct!
-  x.request do
-    x.configuration do
-      x.version 1.0
-      x.key user_key
-      x.host host_key
-    end
-    for entry in entries
-      x.entry do
-        x.key entry[:name].to_s
-        x.value entry[:value].to_f
-        x.max entry[:max].to_f if entry[:max]
-      end
-    end
-  end
-  request.body = xml_output
-
-  # TODO Store for later use if entries were not created
-  Net::HTTP.start(url.host, url.port) {|http| http.request(request) }
+  transmission = Transmission.new(config)
+  transmission.set_entries
+  transmission.send
 end
